0beacb43585cef55b1efd1a4939eec5966fe69a3
Vinicius Resende <resende-vinicius@hotmail.com>
2026-03-19 20:07:35 -0300

    ajusta cors para frontend vercel

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://SEU-FRONTEND.vercel.app"
+        "https://frontend-equipamentos-six.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
