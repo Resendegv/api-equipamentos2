dfcec28882d4a202f21936bc965e79e39cb3a021
Vinicius Resende <resende-vinicius@hotmail.com>
2026-03-19 19:27:16 -0300

    fix deploy render

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, engine
+
+# IMPORTS CORRETOS
 from routers.auth_router import router as auth_router
 from routers.equipamentos_router import router as equipamentos_router
 from routers.manutencoes_router import router as manutencoes_router
 
+# CRIA AS TABELAS
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Fleet Maintenance System")
 
+# CORS (liberar frontend)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -22,11 +26,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# ROTAS
 app.include_router(auth_router)
 app.include_router(equipamentos_router)
 app.include_router(manutencoes_router)
 
-
+# ROTA TESTE
 @app.get("/")
 def home():
-    return {"message": "Fleet Maintenance API online"}
+    return {"message": "Fleet Maintenance API online 🚀"}
